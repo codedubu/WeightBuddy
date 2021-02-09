1361f1f353ab3b2e20418c3731a1da6ade561704
codedubu <mccaine.river@gmail.com>
2021-02-09 13:32:55 -0800

    Initial Commit

---
+++ WeightBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeightBuddy
+//
+//  Created by River McCaine on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
